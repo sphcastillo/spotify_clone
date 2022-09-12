864c36ee915ebbdde365e74b1b2f45a489d7690a
Sophia Castillo <Sphcastillo@gmail.com>
2022-09-12 11:27:42 -0700

    added shuffle from lodash to randomize background color

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
+import Center from '../components/Center';
 import Sidebar from '../components/Sidebar';
 
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
 
-      <main className="">
-        <Sidebar />
-        {/* Center */}
+      <main className="flex">
+          <Sidebar />
+          <Center />
       </main>
 
       <div>{/* Player */}</div>
