3f839d6a430f22199db862dfa9b7e6f7f2c695c6
Sophia Castillo <Sphcastillo@gmail.com>
2022-09-13 10:42:03 -0700

    changes because it's not working

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,6 +6,7 @@ export async function middleware(req){
     // Token will exist if user is logged in
     const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
+
     const { pathname }= req.nextUrl;
     // Allow the request if the following is true...
     // 1. Its a request for next-auth session & provider fetching
@@ -19,5 +20,6 @@ export async function middleware(req){
 
     if(!token && pathname !== "/login"){
         return NextResponse.redirect('/login');
+
     }
 }
